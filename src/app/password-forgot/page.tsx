e77f9bb9fdc12ce09e18a2d14ad1c0aed46da82a
Robin Wieruch <hello@rwieruch.com>
2024-11-15 12:18:50 +0100

    fix(2024-11_password-reset_reset-password): 2024-11_password-reset_reset-password

--- src/app/password-forgot/page.tsx
+++ src/app/password-forgot/page.tsx
@@ -6,7 +6,7 @@ const PasswordForgotPage = () => {
     <div className="flex-1 flex flex-col justify-center items-center">
       <CardCompact
         title="Forgot Password"
-        description="Enter your email address to reset your password."
+        description="Enter your email address to reset your password"
         className="w-full max-w-[420px] animate-fade-from-top"
         content={<PasswordForgotForm />}
       />
