39dafdb6320491daf33ca2b4a916a17862d672e3
Robin Wieruch <hello@rwieruch.com>
2024-11-15 12:35:54 +0100

    fix(2024-11_password-reset_change-password): 2024-11_password-reset_change-password

--- src/app/(authenticated)/account/password/page.tsx
+++ src/app/(authenticated)/account/password/page.tsx
@@ -12,12 +12,14 @@ const PasswordPage = () => {
         tabs={<AccountTabs />}
       />
 
-      <CardCompact
-        title="Change Password"
-        description="Enter your current password"
-        className="w-full max-w-[420px] animate-fade-from-top"
-        content={<PasswordChangeForm />}
-      />
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        <CardCompact
+          title="Change Password"
+          description="Enter your current password"
+          className="w-full max-w-[420px]"
+          content={<PasswordChangeForm />}
+        />
+      </div>
     </div>
   );
 };
