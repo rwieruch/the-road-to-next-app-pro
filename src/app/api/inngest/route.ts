f199fb4be5c3a2751bacea8bd36003ab6b875aad
Robin Wieruch <hello@rwieruch.com>
2024-11-22 12:00:44 +0100

    fix(2024-12_message-queue_receive-events): 2024-12_message-queue_receive-events

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,7 +1,8 @@
 import { serve } from "inngest/next";
+import { passwordResetEvent } from "@/features/password/events/event-password-reset";
 import { inngest } from "@/lib/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [], // TODO: register functions
+  functions: [passwordResetEvent],
 });
