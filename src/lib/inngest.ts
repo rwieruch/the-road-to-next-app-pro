e3d84ebea94501a47b040f5ae0a309ba8ccae24c
Robin Wieruch <hello@rwieruch.com>
2024-11-22 12:04:57 +0100

    fix(2024-12_message-queue_typed-events): 2024-12_message-queue_typed-events

--- src/lib/inngest.ts
+++ src/lib/inngest.ts
@@ -1,3 +1,11 @@
-import { Inngest } from "inngest";
+import { EventSchemas, Inngest } from "inngest";
+import { PasswordResetEventArgs } from "@/features/password/events/event-password-reset";
 
-export const inngest = new Inngest({ id: "the-road-to-next" });
+type Events = {
+  "app/password.password-reset": PasswordResetEventArgs;
+};
+
+export const inngest = new Inngest({
+  id: "the-road-to-next",
+  schemas: new EventSchemas().fromRecord<Events>(),
+});
