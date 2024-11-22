21a3f8f1bc208c9c2716cc014f4a9a26d2ad073d
Robin Wieruch <hello@rwieruch.com>
2024-11-22 13:07:13 +0100

    fix(2024-12-email-verification_send-email-verification): 2024-12-email-verification_send-email-verification

--- src/features/auth/actions/sign-up.ts
+++ src/features/auth/actions/sign-up.ts
@@ -13,6 +13,7 @@ import { createSession } from "@/lib/lucia";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 import { generateRandomToken } from "@/utils/crypto";
+import { sendEmailVerification } from "../emails/send-email-verification";
 import { generateEmailVerificationCode } from "../utils/generate-email-verification-code";
 import { setSessionCookie } from "../utils/session-cookie";
 
@@ -60,7 +61,7 @@ export const signUp = async (_actionState: ActionState, formData: FormData) => {
       user.id,
       email
     );
-    console.log(verificationCode);
+    await sendEmailVerification(username, email, verificationCode);
 
     const sessionToken = generateRandomToken();
     const session = await createSession(sessionToken, user.id);
