ffcdf0ef169a5271b639f3b6464da4da8d14261a
Robin Wieruch <hello@rwieruch.com>
2024-11-22 10:51:15 +0100

    fix(2024-12_password-reset_password-reset-link): 2024-12_password-reset_password-reset-link

--- src/features/password/actions/password-forgot.ts
+++ src/features/password/actions/password-forgot.ts
@@ -7,6 +7,7 @@ import {
   toActionState,
 } from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
+import { generatePasswordResetLink } from "../utils/generate-password-reset-link";
 
 const passwordForgotSchema = z.object({
   email: z.string().min(1, { message: "Is required" }).max(191).email(),
@@ -29,7 +30,11 @@ export const passwordForgot = async (
       return toActionState("ERROR", "Incorrect email", formData);
     }
 
+    const passwordResetLink = await generatePasswordResetLink(user.id);
+
     // TODO: Send email with reset link
+    // instead we will just print it to the console for now
+    console.log(passwordResetLink);
   } catch (error) {
     return fromErrorToActionState(error, formData);
   }
