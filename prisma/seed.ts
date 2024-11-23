7ef2bf22af2ffca69f79c5fc4ddbf7c21ae21544
Robin Wieruch <hello@rwieruch.com>
2024-11-23 22:16:13 +0100

    fix(2024-12_organization_many-to-many): 2024-12_organization_many-to-many

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -54,6 +54,8 @@ const seed = async () => {
   await prisma.comment.deleteMany();
   await prisma.ticket.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.organization.deleteMany();
+  await prisma.membership.deleteMany();
 
   const passwordHash = await hash("geheimnis");
 
@@ -62,6 +64,21 @@ const seed = async () => {
       ...user,
       passwordHash,
     })),
+  });
+
+  const dbOrganization = await prisma.organization.create({
+    data: {
+      name: "Organization 1",
+    },
+  });
+
+  await prisma.membership.create({
+    data: {
+      // for debugging purposes
+      // only one user is added to the organization
+      userId: dbUsers[0].id,
+      organizationId: dbOrganization.id,
+    },
   });
 
   const dbTickets = await prisma.ticket.createManyAndReturn({
