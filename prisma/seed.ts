24f5c31f75fdeb0dc73197095cc04878e12fc050
Robin Wieruch <hello@rwieruch.com>
2024-11-29 14:29:23 +0100

    fix(2024-12_roles_protected-admin-routes): 2024-12_roles_protected-admin-routes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -78,11 +78,13 @@ const seed = async () => {
         userId: dbUsers[0].id,
         organizationId: dbOrganization.id,
         isActive: true,
+        membershipRole: "ADMIN",
       },
       {
         userId: dbUsers[1].id,
         organizationId: dbOrganization.id,
         isActive: false,
+        membershipRole: "MEMBER",
       },
     ],
   });
--- src/features/membership/queries/get-memberships.ts
+++ src/features/membership/queries/get-memberships.ts
@@ -1,10 +1,12 @@
 "use server";
 
-import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+// import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { prisma } from "@/lib/prisma";
+import { getAdminOrRedirect } from "./get-admin-or-redirect";
 
 export const getMemberships = async (organizationId: string) => {
-  await getAuthOrRedirect();
+  await getAdminOrRedirect(organizationId);
+  // await getAuthOrRedirect();
 
   return await prisma.membership.findMany({
     where: {
