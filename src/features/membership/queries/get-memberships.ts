1faab6412a710e4e32a913a760ab6dc0f8d564d0
Robin Wieruch <hello@rwieruch.com>
2024-11-29 16:58:03 +0100

    fix(src/features/membership/queries/get-memberships.ts): src/features/membership/queries/get-memberships.ts

--- src/features/membership/queries/get-memberships.ts
+++ src/features/membership/queries/get-memberships.ts
@@ -1,12 +1,10 @@
 "use server";
 
-// import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { prisma } from "@/lib/prisma";
-import { getAdminOrRedirect } from "./get-admin-or-redirect";
 
 export const getMemberships = async (organizationId: string) => {
-  await getAdminOrRedirect(organizationId);
-  // await getAuthOrRedirect();
+  await getAuthOrRedirect();
 
   return await prisma.membership.findMany({
     where: {
