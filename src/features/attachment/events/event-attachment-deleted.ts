275496d7f69bdf1f16adf6020aeeb4c68ba503a7
Robin Wieruch <hello@rwieruch.com>
2024-12-05 11:59:33 +0100

    fix(2024-12_aws_s3-delete-hotfix): 2024-12_aws_s3-delete-hotfix

--- src/features/attachment/events/event-attachment-deleted.ts
+++ src/features/attachment/events/event-attachment-deleted.ts
@@ -1,11 +1,13 @@
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from "@/lib/aws";
 import { inngest } from "@/lib/inngest";
-import { prisma } from "@/lib/prisma";
 import { generateS3Key } from "../utils/generate-s3-key";
 
 export type AttachmentDeleteEventArgs = {
   data: {
+    organizationId: string;
+    ticketId: string;
+    fileName: string;
     attachmentId: string;
   };
 };
@@ -14,26 +16,20 @@ export const attachmentDeletedEvent = inngest.createFunction(
   { id: "attachment-deleted" },
   { event: "app/attachment.deleted" },
   async ({ event }) => {
-    const { attachmentId } = event.data;
+    const { organizationId, ticketId, fileName, attachmentId } = event.data;
 
     try {
-      const attachment = await prisma.attachment.findUniqueOrThrow({
-        where: {
-          id: attachmentId,
-        },
-        include: {
-          ticket: true,
-        },
-      });
+      // remove
+      // const attachment = await prisma.attachment.findUniqueOrThrow(...);
 
       await s3.send(
         new DeleteObjectCommand({
           Bucket: process.env.AWS_BUCKET_NAME,
           Key: generateS3Key({
-            organizationId: attachment.ticket.organizationId,
-            ticketId: attachment.ticket.id,
-            fileName: attachment.name,
-            attachmentId: attachment.id,
+            organizationId,
+            ticketId,
+            fileName,
+            attachmentId,
           }),
         })
       );
