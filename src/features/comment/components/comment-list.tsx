df671319eb7f1fbf5bd331d9beae9535963ee22a
Robin Wieruch <hello@rwieruch.com>
2024-12-10 23:52:44 +0100

    fix(2024-12_isomorphic_nested-fetch): 2024-12_isomorphic_nested-fetch

--- src/features/comment/components/comment-list.tsx
+++ src/features/comment/components/comment-list.tsx
@@ -1,4 +1,6 @@
 import { AttachmentCreateButton } from "@/features/attachment/components/attachment-create-button";
+import { AttachmentDeleteButton } from "@/features/attachment/components/attachment-delete-button";
+import { AttachmentList } from "@/features/attachment/components/attachment-list";
 import { CommentWithMetadata } from "../types";
 import { CommentDeleteButton } from "./comment-delete-button";
 import { CommentItem } from "./comment-item";
@@ -34,8 +36,31 @@ const CommentList = ({ comments, onDeleteComment }: CommentListProps) => {
             : []),
         ];
 
+        const sections = [];
+
+        if (comment.attachments?.length) {
+          sections.push({
+            label: "Attachments",
+            content: (
+              <AttachmentList
+                attachments={comment.attachments}
+                buttons={(attachmentId) => [
+                  ...(comment.isOwner
+                    ? [<AttachmentDeleteButton key="0" id={attachmentId} />]
+                    : []),
+                ]}
+              />
+            ),
+          });
+        }
+
         return (
-          <CommentItem key={comment.id} comment={comment} buttons={buttons} />
+          <CommentItem
+            key={comment.id}
+            comment={comment}
+            sections={sections}
+            buttons={buttons}
+          />
         );
       })}
     </>
