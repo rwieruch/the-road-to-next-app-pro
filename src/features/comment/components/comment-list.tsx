0c9ecf1f0f4fa92045496dc848cfbfd38d39f600
Robin Wieruch <hello@rwieruch.com>
2024-12-10 23:41:53 +0100

    fix(2024-12_isomorphic_nested-relations): 2024-12_isomorphic_nested-relations

--- src/features/comment/components/comment-list.tsx
+++ src/features/comment/components/comment-list.tsx
@@ -1,3 +1,4 @@
+import { AttachmentCreateButton } from "@/features/attachment/components/attachment-create-button";
 import { CommentWithMetadata } from "../types";
 import { CommentDeleteButton } from "./comment-delete-button";
 import { CommentItem } from "./comment-item";
@@ -11,15 +12,27 @@ const CommentList = ({ comments, onDeleteComment }: CommentListProps) => {
   return (
     <>
       {comments.map((comment) => {
+        const attachmentCreateButton = (
+          <AttachmentCreateButton
+            key="0"
+            entityId={comment.id}
+            entity="COMMENT"
+          />
+        );
+
         const commentDeleteButton = (
           <CommentDeleteButton
-            key="0"
+            key="1"
             id={comment.id}
             onDeleteComment={onDeleteComment}
           />
         );
 
-        const buttons = [...(comment.isOwner ? [commentDeleteButton] : [])];
+        const buttons = [
+          ...(comment.isOwner
+            ? [attachmentCreateButton, commentDeleteButton]
+            : []),
+        ];
 
         return (
           <CommentItem key={comment.id} comment={comment} buttons={buttons} />
