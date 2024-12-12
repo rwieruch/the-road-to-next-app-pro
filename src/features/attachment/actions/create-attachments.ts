7ec921f35c04baaffbcb14442125dad860a7d6c0
Robin Wieruch <hello@rwieruch.com>
2024-12-12 17:14:24 +0100

    fix(2024-12_service_error): 2024-12_service_error

--- src/features/attachment/actions/create-attachments.ts
+++ src/features/attachment/actions/create-attachments.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { AttachmentEntity } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
@@ -11,14 +10,10 @@ import {
 } from "@/components/form/utils/to-action-state";
 import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { isOwner } from "@/features/auth/utils/is-owner";
-import { s3 } from "@/lib/aws";
-import { prisma } from "@/lib/prisma";
 import { ticketPath } from "@/paths";
 import { ACCEPTED, MAX_SIZE } from "../constants";
 import * as attachmentService from "../service";
 import { isComment, isTicket } from "../types";
-import { generateS3Key } from "../utils/generate-s3-key";
-import { getOrganizationIdByAttachment } from "../utils/helpers";
 import { sizeInMB } from "../utils/size";
 
 const createAttachmentsSchema = z.object({
@@ -62,52 +57,18 @@ export const createAttachments = async (
     return toActionState("ERROR", "Not the owner of this subject");
   }
 
-  let attachment;
-
   try {
     const { files } = createAttachmentsSchema.parse({
       files: formData.getAll("files"),
     });
 
-    for (const file of files) {
-      const buffer = await Buffer.from(await file.arrayBuffer());
-
-      attachment = await prisma.attachment.create({
-        data: {
-          name: file.name,
-          ...(entity === "TICKET" ? { ticketId: entityId } : {}),
-          ...(entity === "COMMENT" ? { commentId: entityId } : {}),
-          entity,
-        },
-      });
-
-      const organizationId = getOrganizationIdByAttachment(entity, subject);
-
-      await s3.send(
-        new PutObjectCommand({
-          Bucket: process.env.AWS_BUCKET_NAME,
-          Key: generateS3Key({
-            organizationId,
-            entityId,
-            entity,
-            fileName: file.name,
-            attachmentId: attachment.id,
-          }),
-          Body: buffer,
-          ContentType: file.type,
-        })
-      );
-    }
+    await attachmentService.createAttachments({
+      subject,
+      entity,
+      entityId,
+      files,
+    });
   } catch (error) {
-    // fallback if S3 upload fails, but attachment was created
-    if (attachment) {
-      await prisma.attachment.delete({
-        where: {
-          id: attachment.id,
-        },
-      });
-    }
-
     return fromErrorToActionState(error);
   }
 
