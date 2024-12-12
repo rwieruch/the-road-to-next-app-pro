9bbeac94a34ac23cc2d9ba8770a6abd8978c3328
Robin Wieruch <hello@rwieruch.com>
2024-12-12 17:18:26 +0100

    fix(2024-12_service_reuse): 2024-12_service_reuse

--- src/features/comment/actions/create-comment.tsx
+++ src/features/comment/actions/create-comment.tsx
@@ -8,6 +8,7 @@ import {
   toActionState,
 } from "@/components/form/utils/to-action-state";
 import { filesSchema } from "@/features/attachment/schema/files";
+import * as attachmentService from "@/features/attachment/service";
 import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { prisma } from "@/lib/prisma";
 import { ticketPath } from "@/paths";
@@ -27,17 +28,32 @@ export const createComment = async (
   let comment;
 
   try {
-    const data = createCommentSchema.parse(Object.fromEntries(formData));
+    const { content, files } = createCommentSchema.parse({
+      content: formData.get("content"),
+      files: formData.getAll("files"),
+    });
 
     comment = await prisma.comment.create({
       data: {
         userId: user.id,
-        ticketId: ticketId,
-        ...data,
+        ticketId,
+        content,
       },
       include: {
-        user: true,
+        user: {
+          select: {
+            username: true,
+          },
+        },
+        ticket: true,
       },
+    });
+
+    await attachmentService.createAttachments({
+      subject: comment,
+      entity: "COMMENT",
+      entityId: comment.id,
+      files,
     });
   } catch (error) {
     return fromErrorToActionState(error);
