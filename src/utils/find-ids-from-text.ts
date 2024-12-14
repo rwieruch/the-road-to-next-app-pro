f024203a8593a0fe00bc4788dbaa0fa566506fd0
Robin Wieruch <hello@rwieruch.com>
2024-12-14 16:44:24 +0100

    fix(2024-12_self-relation_regex): 2024-12_self-relation_regex

--- src/utils/find-ids-from-text.ts
+++ src/utils/find-ids-from-text.ts
@@ -1,4 +1,6 @@
-export const findTicketIdsFromText = (key: string, value: string) => {
+type FeatureKey = "tickets";
+
+export const findTicketIdsFromText = (key: FeatureKey, value: string) => {
   const regexPattern = new RegExp(`/${key}/[a-zA-Z0-9]+`, "g");
   const paths = value.match(regexPattern) || [];
   return paths.map((path) => path.replace(`/${key}/`, ""));
