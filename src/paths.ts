b4d66c13bd7984a07cef7559e79e552085d375d7
Robin Wieruch <hello@rwieruch.com>
2025-03-17 13:20:49 +0100

    fix(qwd): qwd

--- src/paths.ts
+++ src/paths.ts
@@ -17,6 +17,8 @@ export const organizationCreatePath = () => "/organization/create";
 
 export const membershipsPath = (organizationId: string) =>
   `/organization/${organizationId}/memberships`;
+export const invitationsPath = (organizationId: string) =>
+  `/organization/${organizationId}/invitations`;
 
 export const accountProfilePath = () => "/account/profile";
 export const accountPasswordPath = () => "/account/password";
