28b2bd3f89ede3e4e3a6a45e99e8f1bf0a9130cb
Robin Wieruch <hello@rwieruch.com>
2025-03-19 16:43:27 +0100

    fix(qwd): qwd

--- src/paths.ts
+++ src/paths.ts
@@ -28,3 +28,6 @@ export const ticketsPath = () => "/tickets";
 export const ticketsByOrganizationPath = () => "/tickets/organization";
 export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`;
 export const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`;
+
+export const attachmentDownloadPath = (attachmentId: string) =>
+  `/api/aws/s3/attachments/${attachmentId}`;
