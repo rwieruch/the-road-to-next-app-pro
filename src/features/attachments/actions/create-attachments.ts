34d8f507f1e80f6246cecffebd846d4a22076a99
Robin Wieruch <hello@rwieruch.com>
2025-03-19 14:18:05 +0100

    fix(qwd): qwd

--- src/features/attachments/actions/create-attachments.ts
+++ src/features/attachments/actions/create-attachments.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import {
@@ -9,11 +10,27 @@ import {
 } from "@/components/form/utils/to-action-state";
 import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { isOwner } from "@/features/auth/utils/is-owner";
+import { s3 } from "@/lib/aws";
 import { prisma } from "@/lib/prisma";
 import { ticketPath } from "@/paths";
+import { ACCEPTED, MAX_SIZE } from "../constants";
+import { generateS3Key } from "../utils/generate-s3-key";
+import { sizeInMB } from "../utils/size";
 
 const createAttachmentsSchema = z.object({
-  files: z.custom<FileList>().transform((files) => Array.from(files)),
+  files: z
+    .custom<FileList>()
+    .transform((files) => Array.from(files))
+    .transform((files) => files.filter((file) => file.size > 0))
+    .refine(
+      (files) => files.every((file) => sizeInMB(file.size) <= MAX_SIZE),
+      `The maximum file size is ${MAX_SIZE}MB`
+    )
+    .refine(
+      (files) => files.every((file) => ACCEPTED.includes(file.type)),
+      "File type is not supported"
+    )
+    .refine((files) => files.length !== 0, "File is required"),
 });
 
 export const createAttachments = async (
@@ -45,8 +62,26 @@ export const createAttachments = async (
     for (const file of files) {
       const buffer = await Buffer.from(await file.arrayBuffer());
 
-      // TODO: upload to S3
-      // TODO: create a database reference to S3 file
+      const attachment = await prisma.attachment.create({
+        data: {
+          name: file.name,
+          ticketId,
+        },
+      });
+
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: generateS3Key({
+            organizationId: ticket.organizationId,
+            ticketId,
+            fileName: file.name,
+            attachmentId: attachment.id,
+          }),
+          Body: buffer,
+          ContentType: file.type,
+        })
+      );
     }
   } catch (error) {
     return fromErrorToActionState(error);
