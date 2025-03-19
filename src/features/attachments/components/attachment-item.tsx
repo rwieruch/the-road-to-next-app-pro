5575cc5bde823d5b3ad322bce591755441007559
Robin Wieruch <hello@rwieruch.com>
2025-03-19 15:32:50 +0100

    fix(qwd): qw
    d

--- src/features/attachments/components/attachment-item.tsx
+++ src/features/attachments/components/attachment-item.tsx
@@ -2,10 +2,16 @@ import { Attachment } from "@prisma/client";
 
 type AttachmentItemProps = {
   attachment: Attachment;
+  buttons: React.ReactNode[];
 };
 
-const AttachmentItem = ({ attachment }: AttachmentItemProps) => {
-  return <p className="text-sm">{attachment.name}</p>;
+const AttachmentItem = ({ attachment, buttons }: AttachmentItemProps) => {
+  return (
+    <div className="flex justify-between items-center">
+      <p className="text-sm truncate">{attachment.name}</p>
+      {buttons}
+    </div>
+  );
 };
 
 export { AttachmentItem };
