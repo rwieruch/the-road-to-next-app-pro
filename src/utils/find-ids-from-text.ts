d93c0a49e1b4b0e7bbf082f661e455fbd4be6261
Robin Wieruch <hello@rwieruch.com>
2025-04-01 15:52:40 +0200

    fix(qwd): qw
    d

--- src/utils/find-ids-from-text.ts
+++ src/utils/find-ids-from-text.ts
@@ -1,6 +1,6 @@
 type FeatureKey = "tickets";
 
-export const findTicketIdsFromText = (key: FeatureKey, value: string) => {
+export const findIdsFromText = (key: FeatureKey, value: string) => {
   const regexPattern = new RegExp(`/${key}/[a-zA-Z0-9]+`, "g");
   const paths = value.match(regexPattern) || [];
   return paths.map((path) => path.replace(`/${key}/`, ""));
